c861e68eaf4a82ed9a2fdb905bcd0b6f138bbc41
Neil Northrop <nnorthrop@gmail.com>
2013-06-09 10:53:29 -0700

    Worked on adding question variables

--- hello.rb
+++ hello.rb
@@ -1,27 +1,20 @@
 require 'pp'
 =begin
+class with an initialize
+def File.open
+def add game
+def sub game
+def rename game
 array of board games
 rand generator
 =end
-		file_board_games = File.read('list_of_board_games.txt').split(',')# { |word| word.split(',')}
 
-		array_of_games = file_board_games.map do |word|
-			word.gsub(/^'|'$/, '')
-		end
+file_board_games = File.read('list_of_board_games.txt').split(',').sort# { |word| word.split(',')}
 
-		# puts "Do you have a new game to put in?"
-		# puts "Type 'yes' or 'no'"
-		# response = gets.chomp.downcase
-		# 	if response == 'yes'
-		# 		puts "Enter new board game: "
-		# 		new_game = gets.chomp
-		# 		list << new_game
-		# 	elsif response == 'no'
-		# 			add_game
-		# 	else
-		# 		puts "Type 'yes' or 'no'"
-		# 		return true
-		# 	end
+array_of_games = file_board_games.map do |word|
+	word.gsub(/^'|'$/, '')
+end
+
 puts
 number_in_list = array_of_games.length
 puts number_in_list
@@ -31,9 +24,24 @@ rand_game = rand(number_in_list)
 puts rand_game
 
 array_of_games.each_index do |array_index|
-	if array_index == rand_game
-		print array_of_games[rand_game]
-	end
+	puts array_of_games[rand_game] if array_index == rand_game
 end
+
+puts "Do you have a new game to put in?"
+puts "Type 'yes' or 'no'"
+response = gets.chomp.downcase
+	if response == 'yes'
+		puts "Enter new board game: "
+		new_game = gets.chomp
+		array_of_games << new_game
+		print array_of_games.sort
+	elsif response == 'no'
+			return false
+	else
+		puts "Type 'yes' or 'no'"
+		return true
+	end
+
+
 
 
